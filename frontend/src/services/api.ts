bff8912e765084df9806cc7766c1270dd09027a2
chimdos <d4ni.martis.24@gmail.com>
2026-01-13 17:22:37 -0300

    chore: remove os comentários do api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import './apiInterceptor';
 import { setupInterceptors } from './apiInterceptor';
 
-// Cria uma instância do axios com a URL base da nossa API
 const api = axios.create({
   baseURL: 'https://fashionhub-backend-yuya.onrender.com',
 });
