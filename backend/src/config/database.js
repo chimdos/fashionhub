1df9711649a3ddb0fc366cf59a43dbeee9aaed3f
chimdos <d4ni.martis.24@gmail.com>
2026-01-19 14:45:56 -0300

    fix(database): cria a variável isProduction que não existia, é usada no logging do config e bugava o deploy por não existir

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const config = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
