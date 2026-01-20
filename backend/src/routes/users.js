a9fec123663e9dfe38798dfe0bc1ce6931d5aa7b
chimdos <d4ni.martis.24@gmail.com>
2026-01-20 15:50:01 -0300

    feat(routes): adiciona a função changePassword a routes users.js

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -9,5 +9,6 @@ router.put('/become-courier', authMiddleware, userController.becomeCourier);
 router.put('/store/profile', authMiddleware, userController.updateStoreProfile);
 router.put('/responsible', authMiddleware, userController.updateResponsibleData);
 router.put('/store/address', authMiddleware, userController.updateStoreAddress);
+router.put('/change-password', authMiddleware, userController.changePassword);
 
 module.exports = router;
