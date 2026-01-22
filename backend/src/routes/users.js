77f1d4fbc7af6c689c1ed5c4753ee3ca09f0c1c1
chimdos <d4ni.martis.24@gmail.com>
2026-01-22 14:16:11 -0300

    fix(routes): altera a ordem das rotas /:id e /become-courier para impedir duplicidade dos puts com id e deixar a rota de variável/generalista como última

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -4,11 +4,11 @@ const userController = require('../controllers/userController');
 const { authMiddleware } = require('../middleware/auth');
 
 router.get('/me', authMiddleware, userController.getCurrentUserProfile);
-router.put('/:id', authMiddleware, userController.updateUser);
 router.put('/become-courier', authMiddleware, userController.becomeCourier);
 router.put('/store/profile', authMiddleware, userController.updateStoreProfile);
 router.put('/responsible', authMiddleware, userController.updateResponsibleData);
 router.put('/store/address', authMiddleware, userController.updateStoreAddress);
 router.put('/change-password', authMiddleware, userController.changePassword);
+router.put('/:id', authMiddleware, userController.updateUser);
 
 module.exports = router;
