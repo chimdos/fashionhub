54610d2018b3d750e6c15466a2e60ec384275ab8
chimdos <d4ni.martis.24@gmail.com>
2026-01-22 15:56:02 -0300

    feat(navigation): refatora o CourierNavigator e adiciona as novas telas

--- frontend/src/navigation/CourierNavigator.tsx
+++ frontend/src/navigation/CourierNavigator.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { CourierDashboardScreen } from '../screens/courier/CourierDashboardScreen';
+import { CourierTabNavigator } from './CourierTabNavigator';
 import { PickupScreen } from '../screens/courier/PickupScreen';
 import { DeliveryRouteScreen } from '../screens/courier/DeliveryRouteScreen';
-import { LoginScreen } from '../screens/Auth/LoginScreen';
-import { RegisterScreen } from '../screens/Auth/RegisterScreen';
-import { useAuth } from '../contexts/AuthContext';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,29 +10,20 @@ export function CourierNavigator() {
   return (
     <Stack.Navigator>
       <Stack.Screen 
-        name="CourierDashboard" 
-        component={CourierDashboardScreen} 
-        options={{ title: 'Painel de Entregas' }} 
+        name="MainTabs" 
+        component={CourierTabNavigator} 
+        options={{ headerShown: false }} 
       />
+      
       <Stack.Screen 
         name="PickupScreen" 
         component={PickupScreen} 
-        options={{ title: 'Retirada na Loja' }} 
+        options={{ title: 'Retirar Mala' }} 
       />
       <Stack.Screen 
         name="DeliveryRoute" 
         component={DeliveryRouteScreen} 
         options={{ title: 'Rota de Entrega' }} 
-      />
-      <Stack.Screen 
-        name="LoginScreen" 
-        component={LoginScreen} 
-        options={{ headerShown: false }} 
-      />
-       <Stack.Screen 
-        name="RegisterScreen" 
-        component={RegisterScreen} 
-        options={{ headerShown: false }} 
       />
     </Stack.Navigator>
   );
