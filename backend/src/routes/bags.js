371b6ff9da542c4ce3cef573d4e4f3762555c4a6
chimdos <d4ni.martis.24@gmail.com>
2026-01-23 16:59:32 -0300

    feat(routes): adiciona a active-deliveries às rotas do bags.js

--- backend/src/routes/bags.js
+++ backend/src/routes/bags.js
@@ -3,57 +3,19 @@ const router = express.Router();
 const bagController = require('../controllers/bagController');
 const { authMiddleware, requireUserType } = require('../middleware/auth');
 
-router.get(
-  '/store',
-  authMiddleware,
-  requireUserType('lojista'),
-  bagController.getStoreBagRequests
-);
-
-router.get('/', authMiddleware, bagController.getClientBags);
-
-router.post(
-  '/',
-  authMiddleware,
-  requireUserType('cliente'),
-  bagController.createBagRequest
-);
-
+router.get('/store', authMiddleware, requireUserType('lojista'), bagController.getStoreBagRequests);
 router.get('/available', authMiddleware, bagController.getAvailableDeliveries);
+router.get('/active-deliveries', authMiddleware, bagController.getActiveDeliveries);
+router.get('/', authMiddleware, bagController.getClientBags);
+router.post('/', authMiddleware, requireUserType('cliente'), bagController.createBagRequest);
 
 router.get('/:bagId', authMiddleware, bagController.getBagById);
-
-router.post(
-  '/:bagId/confirm-purchase',
-  authMiddleware,
-  requireUserType('cliente'),
-  bagController.confirmPurchase
-);
-
-router.post(
-  '/:bagId/store-action',
-  authMiddleware,
-  requireUserType('lojista'),
-  bagController.updateStatusByStore
-);
-
+router.post('/:bagId/confirm-purchase', authMiddleware, requireUserType('cliente'), bagController.confirmPurchase);
+router.post('/:bagId/store-action', authMiddleware, requireUserType('lojista'), bagController.updateStatusByStore);
 router.post('/:bagId/accept', authMiddleware, requireUserType('entregador'), bagController.acceptDelivery);
-
-router.post(
-  '/:bagId/request-courier', 
-  authMiddleware, 
-  requireUserType('lojista'), 
-  bagController.requestCourier
-);
-
+router.post('/:bagId/request-courier', authMiddleware, requireUserType('lojista'), bagController.requestCourier);
 router.post('/:bagId/confirm-pickup', authMiddleware, bagController.confirmPickup);
 router.post('/:bagId/confirm-delivery', authMiddleware, bagController.confirmDelivery);
-
-router.put(
-  '/:bagId/confirm-return',
-  authMiddleware,
-  requireUserType('lojista'),
-  bagController.confirmReturn
-);
+router.put('/:bagId/confirm-return', authMiddleware, requireUserType('lojista'), bagController.confirmReturn);
 
 module.exports = router;
