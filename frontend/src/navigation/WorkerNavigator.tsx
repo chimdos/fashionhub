66d4e588f83d74af283bd0fa798aa5e78495ffbc
chimdos <d4ni.martis.24@gmail.com>
2026-02-03 15:45:00 -0300

    feat(navigation): adiciona a BagDetailScreen ao WorkerNavigator

--- frontend/src/navigation/WorkerNavigator.tsx
+++ frontend/src/navigation/WorkerNavigator.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeWorkerScreen } from '../screens/worker/HomeWorkerScreen';
+import { BagDetailScreen } from '../screens/store/BagDetailScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -8,6 +9,7 @@ export const WorkerNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="HomeWorker" component={HomeWorkerScreen} />
+      <Stack.Screen name="BagDetail" component={BagDetailScreen} />
     </Stack.Navigator>
   );
 };
