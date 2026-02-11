0721018217bdf9a073462857ac11af85b9df5f77
chimdos <d4ni.martis.24@gmail.com>
2026-02-11 15:29:02 -0300

    feat(controllers): cria a função authorizePayment para o Hold

--- backend/src/controllers/transactionController.js
+++ backend/src/controllers/transactionController.js
@@ -8,7 +8,7 @@ const transactionController = {
       const transactions = await Transaction.findAll({
         where: { cliente_id },
         include: [
-            { model: Bag, as: 'mala' }
+          { model: Bag, as: 'mala' }
         ],
         order: [['data_transacao', 'DESC']]
       });
@@ -17,6 +17,29 @@ const transactionController = {
     } catch (error) {
       console.error('Erro ao buscar transações:', error);
       res.status(500).json({ message: 'Erro interno do servidor' });
+    }
+  },
+
+  async authorizePayment(bag, cliente_id, t) {
+    try {
+      const valorCaucao = 50.00;
+
+      // api do gateway
+
+      const transaction = await Transaction.create({
+        cliente_id,
+        mala_id: bag.id,
+        valor_total: valorCaucao,
+        status_pagamento: 'processando',
+        tipo: 'autorizacao',
+        metodo_pagamento: 'cartao_credito',
+        gateway_id: 're_123456'
+      }, { transaction: t });
+
+      return transaction;
+    } catch (error) {
+      console.error('Erro na autorização:', error);
+      throw new Error('Falha na pré-autorização do cartão.');
     }
   },
 };
