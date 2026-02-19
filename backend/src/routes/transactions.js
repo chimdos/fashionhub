9090b94adf74c48fd691a2b2877e13e152adb8cb
chimdos <d4ni.martis.24@gmail.com>
2026-02-19 16:00:01 -0300

    fix(routes): adiciona produtos, variacoes e itensMala ao routes transactions.js dentro do debug-ids

--- backend/src/routes/transactions.js
+++ backend/src/routes/transactions.js
@@ -18,13 +18,16 @@ router.post(
 );
 
 router.get('/debug-ids', async (req, res) => {
-  const { User, Bag, Loja } = require('../models');
-  
+  const { User, Bag, Loja, BagItem, Product, ProductVariation } = require('../models');
+
   const usuarios = await User.findAll({ attributes: ['id', 'nome', 'email'] });
   const malas = await Bag.findAll({ attributes: ['id', 'status'] });
   const lojas = await Loja.findAll({ attributes: ['id', 'nome_loja'] });
+  const produtos = await Product.findAll({ attributes: ['id', 'nome', 'preco'] });
+  const variacoes = await ProductVariation.findAll({ attributes: ['id', 'produto_id', 'tamanho', 'cor'] });
+  const itensMala = await BagItem.findAll({ attributes: ['id', 'mala_id', 'variacao_produto_id', 'status_item'] });
 
-  res.json({ usuarios, malas, lojas });
+  res.json({ usuarios, malas, lojas, produtos, variacoes, itensMala });
 });
 
 module.exports = router;
