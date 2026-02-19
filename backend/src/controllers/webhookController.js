623690c6b038bd49e4f18f59986586aeb449a833
chimdos <d4ni.martis.24@gmail.com>
2026-02-19 11:45:19 -0300

    feat(controllers): cria uma função auxiliar de geração de token no webhookController

--- backend/src/controllers/webhookController.js
+++ backend/src/controllers/webhookController.js
@@ -4,6 +4,8 @@ const { Payment, MercadoPagoConfig } = require('mercadopago');
 const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN });
 const payment = new Payment(client);
 
+const generateToken = () => Math.floor(100000 + Math.random() * 900000).toString();
+
 const webhookController = {
     async handle(req, res) {
         const { action, data } = req.body;
